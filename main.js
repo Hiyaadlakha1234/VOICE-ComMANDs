d91722aaf9e9dfbd09687fa1812e71414b6e58cd
Hiyaadlakha1234 <98635114+Hiyaadlakha1234@users.noreply.github.com>
2023-07-31 20:39:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,70 @@
-rightWristX = "";
-leftWristX = "";
-square_length="";
+x = 0;
+y = 0;
+screen_width = 0;
+screen_height = 0;
+apple = "";
+speak_data = "";
+to_number = "";
+
+draw_apple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start() {
+  background("pink");
+  document.getElementById("status").innerHTML = "System is listening please speak";
+  recognition.start();
+}
+
+recognition.onresult = function (event) {
+
+  console.log(event);
+
+  content = event.results[0][0].transcript;
+
+  document.getElementById("status").innerHTML = "The speech has been recognized: " + content;
+
+  to_number = Number(content);
+  if (Number.isInteger(to_number)) {
+    document.getElementById("status").innerHTML = "Started drawing apple  ";
+    draw_apple = "set";
+  }
+
+}
+
 function setup() {
-    video = createCapture(VIDEO);
-    video.size(550, 500);
-    canvas = createCanvas(550, 450);
-    canvas.position(620, 150);
-    posenett = ml5.poseNet(video, modelLoaded);
+  screen_width = window.innerWidth-100;
+  screen_height = window.innerHeight;
+  canvas = createCanvas(screen_width, screen_height - 150);
 }
+
 function draw() {
-   background("black");
-   stroke("white");
-   fill("white");
-   textSize(square_length);
-   text("HIYA",100,250);
-}
-function modelLoaded() {
-    console.log("Pose Net Loaded Successfully");
-    posenett.on("pose", getResult)
-}
-function getResult(r) {
-    if (r.length > 0) {
-        console.log(r);
-        leftWristX = r[0].pose.leftWrist.x;
-        rightWristX = r[0].pose.rightWrist.x;
-        square_length=leftWristX-rightWristX;
-        document.getElementById("font_size").innerHTML=round(square_length)+" px ";
+  if (draw_apple == "set") {
+    for (let i = 1; i < to_number; i++) {
+      x = Math.floor(Math.random() * screen_width-50);
+      y = Math.floor(Math.random() * screen_height-250);
+      image(apple, x, y, 50, 50);
+
     }
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+    speak_data = to_number + " Apples drawn";
+    speak();
+  }
+
+}
+
+function speak() {
+  var synth = window.speechSynthesis;
+
+  var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+  synth.speak(utterThis);
+
+  speak_data = "";
+}
+function preload() {
+  apple = loadImage("apple.png");
 }
